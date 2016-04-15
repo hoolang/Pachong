44d04fbb8bc310e3e425895a247111f19892d615
Circle <wop129@tom.com>
2016-04-15 16:58:06 +0800

    实现ppkoo抓取基本信息

--- src/GithubRepo.java
+++ src/GithubRepo.java
@@ -1,14 +1,16 @@
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Task;
 import us.codecraft.webmagic.model.ConsolePageModelPipeline;
 import us.codecraft.webmagic.model.OOSpider;
 import us.codecraft.webmagic.model.annotation.ExtractBy;
 import us.codecraft.webmagic.model.annotation.ExtractByUrl;
 import us.codecraft.webmagic.model.annotation.HelpUrl;
 import us.codecraft.webmagic.model.annotation.TargetUrl;
+import us.codecraft.webmagic.pipeline.PageModelPipeline;
 
 @TargetUrl("https://github.com/\\w+/\\w+")
 @HelpUrl("https://github.com/\\w+")
-public class GithubRepo {
+public class GithubRepo implements PageModelPipeline<Object>{
 
     @ExtractBy(value = "//h1[@class='entry-title public']/strong/a/text()", notNull = true)
     private String name;
@@ -27,4 +29,14 @@ public class GithubRepo {
         
         System.out.println(repo.name);
     }
+
+	@Override
+	public void process(Object arg0, Task arg1) {
+		System.out.println("=======");
+		GithubRepo repo = (GithubRepo)arg0;
+		System.out.println(repo.name + "=======");
+		System.out.println(repo.author + "=======");
+		System.out.println(repo.readme + "=======");
+		
+	}
 }
--- src/SinaBlogProcessor.java
+++ src/SinaBlogProcessor.java
@@ -2,36 +2,28 @@ import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
+import us.codecraft.webmagic.selector.Html;
 
 /**
  * @author code4crafter@gmail.com <br>
  */
 public class SinaBlogProcessor implements PageProcessor {
 
-    public static final String URL_LIST = "http://blog\\.sina\\.com\\.cn/s/articlelist_1487828712_0_\\d+\\.html";
-
-    public static final String URL_POST = "http://blog\\.sina\\.com\\.cn/s/blog_\\w+\\.html";
-
     private Site site = Site
             .me()
-            .setDomain("blog.sina.com.cn")
+            .setDomain("www.ppkoo.com")
             .setSleepTime(3000)
             .setUserAgent(
                     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
 
     @Override
     public void process(Page page) {
-        //列表页
-        if (page.getUrl().regex(URL_LIST).match()) {
-            page.addTargetRequests(page.getHtml().xpath("//div[@class=\"articleList\"]").links().regex(URL_POST).all());
-            page.addTargetRequests(page.getHtml().links().regex(URL_LIST).all());
-            //文章页
-        } else {
-            page.putField("title", page.getHtml().xpath("//div[@class='articalTitle']/h2"));
-            page.putField("content", page.getHtml().xpath("//div[@id='articlebody']//div[@class='articalContent']"));
-            page.putField("date",
-                    page.getHtml().xpath("//div[@id='articlebody']//span[@class='time SG_txtc']").regex("\\((.*)\\)"));
-        }
+    	page.putField("title", page.getHtml().xpath("h1/text()"));
+    	page.putField("price", page.getHtml().xpath("span[@id='js-price']//text()"));
+    	//<div class="cons_main_rt_c"><div class="cons_m"><ul class="info">
+    	page.putField("description", page.getHtml().xpath("//div[@class='cons_main_rt_c']/div[@class='cons_m']/ul[@class='info']/li/text()").all());
+    	//<div class="b-thumb" id="thumblist"><ul>
+    	page.putField("images",page.getHtml().xpath("//div[@id='thumblist']/ul/li/a/img/@src").regex("(.*)_60x60.jpg").all());
     }
 
     @Override
@@ -40,7 +32,7 @@ public class SinaBlogProcessor implements PageProcessor {
     }
 
     public static void main(String[] args) {
-        Spider.create(new SinaBlogProcessor()).addUrl("http://blog.sina.com.cn/s/articlelist_1487828712_0_1.html")
+        Spider.create(new SinaBlogProcessor()).addUrl("http://www.ppkoo.com/product/5134409.html")
                 .run();
     }
 }
